c7e9113fef45e43bbfc3f6a8029b55ae0b8bf336
NALAGARLA MANOJ <manojnalagarla@gmail.com>
2026-05-06 20:07:43 +0530

    Add files via upload

---
+++ LoginSessionManagement/src/main/java/com/ecommerce/servlet/DashBoardServlet.java
@@ -0,0 +1,54 @@
+package com.ecommerce.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class DashBoardServlet
+ */
+@WebServlet("/dashboard")
+public class DashBoardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DashBoardServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+       // HttpSession session = request.getSession(false);
+
+       // if (session == null || session.getAttribute("user") == null) {
+          //  response.sendRedirect("login.jsp");
+          //  return;
+        //}
+
+        RequestDispatcher rd = request.getRequestDispatcher("dashboard.jsp");
+        rd.forward(request, response);
+    
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
